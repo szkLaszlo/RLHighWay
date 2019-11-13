ede6e89458ea454602b7da90957ac54d2a086e15
laci <szkLaszlo>
2019-11-13 15:12:29 +0100

    091437

--- sumoProject/agents/train_policyGrad.py
+++ sumoProject/agents/train_policyGrad.py
@@ -4,7 +4,6 @@ from sumoProject.agents.policyGradient import Policy
 
 if __name__ == "__main__":
     env = gym.make('EPHighWay-v1')
-    env.set_reward_type('complex')
     env.render(mode='none')
     # Hyperparameters
     learning_rate = 0.0001
--- sumoProject/envs/SUMO_Starter.py
+++ sumoProject/envs/SUMO_Starter.py
@@ -18,7 +18,7 @@ class EPHighWayEnv(gym.Env):
 
     def __init__(self):
 
-        self.max_punishment = -100
+        self.max_punishment = -20
         self.steps_done = 0
         self.rendering = None
 
