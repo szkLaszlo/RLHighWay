385d907a610672012d509bd3cbf3d7bf8ba4cc48
Szőke László <szoke.laszlo95@gmail.com>
2020-02-10 13:53:53 +0100

    added find latest weight

--- sumoProject/agents/eval_policyGrad.py
+++ sumoProject/agents/eval_policyGrad.py
@@ -1,10 +1,31 @@
-import easygui as easygui
+import os
+
 import gym
 
 from sumoProject.agents.policyGradient import Policy
 
+
+def find_latest_weight(path='torchSummary'):
+    """
+
+    :param path:
+    :return:
+    """
+    name_list = os.listdir(path)
+    full_list = [os.path.join(path, i) for i in name_list]
+    time_sorted_list = sorted(full_list, key=os.path.getmtime)
+    if os.path.isdir(time_sorted_list[-1]):
+        latest_weight = find_latest_weight(path=time_sorted_list[-1])
+        return latest_weight
+    else:
+        for i in range(1, len(time_sorted_list) + 1):
+            if time_sorted_list[-i].endswith('.weight'):
+                return time_sorted_list[-i]
+    print(time_sorted_list)
+
 episode_nums = 100
-path = easygui.fileopenbox()
+# path = easygui.fileopenbox()
+path = find_latest_weight()
 env = gym.make('EPHighWay-v1')
 env.render('human')
 policy = Policy(env=env, tb_summary=False)
